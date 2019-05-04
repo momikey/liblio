317e474792cdc6a259f3097cc7b0875c5a1902b1
Michael H. Potter <michael@potterpcs.net>
2019-05-04 02:26:32 -0400

    Added Buefy, changed browserslist options

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -27,6 +27,10 @@
       <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
       <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
     </ul>
+    
+    <section>
+        <b-button @click="clickMe">Click Me!</b-button>
+    </section>
   </div>
 </template>
 
@@ -35,6 +39,11 @@ export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  methods: {
+    clickMe() {
+        this.$notification.open('Clicked!')
+    }
   }
 }
 </script>
