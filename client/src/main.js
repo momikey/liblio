0733baf5dc7623d2163b46c64a984b1a5ed0226b
Michael H. Potter <michael@potterpcs.net>
2019-05-05 00:21:38 -0400

    Added vue-resource

--- client/src/main.js
+++ client/src/main.js
@@ -1,4 +1,5 @@
 import Vue from 'vue'
+import VueResource from 'vue-resource'
 import App from './App.vue'
 import router from './router'
 import Buefy from 'buefy'
@@ -7,6 +8,7 @@ import 'buefy/dist/buefy.css'
 Vue.config.productionTip = false
 
 Vue.use(Buefy)
+Vue.use(VueResource)
 
 new Vue({
   router,
