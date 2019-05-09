9434e146e058e524b7768729b580bdf97f99cccd
Michael H. Potter <michael@potterpcs.net>
2019-05-09 05:55:54 -0400

    Started work on the creator panel component

---
+++ client/src/components/CreatorPanel.vue
@@ -0,0 +1,42 @@
+<template>
+    <section id="creator-panel" class="section">
+        <div class="container is-fluid">
+            <div class="level">
+                <!-- User avatar -->
+                <div class="level-item">
+                    <figure class="image is-128x128" id="avatar-figure">
+                        <img class="is-rounded" src="../assets/logo.png" />
+                    </figure>
+                </div>
+            </div>
+
+            <div class="level">
+                <!-- User name -->
+                <div class="level-item has-text-centered">
+                    <div>
+                    <h1 class="is-size-4">Your name here</h1>
+                    <h2 class="is-size-6">(@yourname@example.com)</h2>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style lang="scss" scoped>
+#creator-panel {
+    background-color: #ffeedd;
+    height: 100%;
+    padding-top: 32px;
+}
+
+#avatar-figure > img {
+    background-color: gray;
+}
+</style>
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,18 +1,23 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="home columns">
+    <div class="column is-two-thirds">
+      <img alt="Vue logo" src="../assets/logo.png">
+      <p>We'll put posts and things like that over here</p>
+    </div>
+
+    <div class="column is-one-third">
+      <creator-panel />
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import CreatorPanel from '@/components/CreatorPanel.vue'
 
 export default {
   name: 'home',
   components: {
-    HelloWorld
+    CreatorPanel
   }
 }
 </script>
