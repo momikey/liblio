5de32672d9e20d36007a7dc531d36a1bdf0f792d
Michael H. Potter <michael@potterpcs.net>
2019-08-15 02:19:31 -0400

    Initialized migrations

--- server/liblio/__init__.py
+++ server/liblio/__init__.py
@@ -4,6 +4,8 @@ import os
 
 from flask import Flask
 from flask_cors import CORS
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 def create_app():
     # Create the app
@@ -18,14 +20,22 @@ def create_app():
     )
 
     # Other config stuff goes here
+    app.config.from_pyfile('config.py', silent=True)
+
     if app.env == 'production':
-        app.config.from_pyfile('config.py', silent=True)
+        app.config.from_pyfile('prod.py', silent=True)
+    else:
+        app.config.from_pyfile('dev.py', silent=True)
 
     # Create the instance folder if it doesn't exist
     try:
         os.makedirs(app.instance_path)
     except OSError:
         pass
+    
+    # Database and migrations
+    db = SQLAlchemy(app)
+    migrate = Migrate(app, db)
 
     # Routes (we'll factor these out later)
     @app.route("/")
