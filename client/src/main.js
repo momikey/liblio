53a184e2987a13c857205e28b860f39dc72fda3b
Michael H. Potter <michael@potterpcs.net>
2019-09-02 17:43:24 -0400

    Added vue-i18n, updated deps

--- client/src/main.js
+++ client/src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import store from './store'
 import router from './router'
 import Buefy from 'buefy'
+import i18n from './i18n'
 // import 'buefy/dist/buefy.css'
 
 Vue.config.productionTip = false
@@ -12,5 +13,6 @@ Vue.use(Buefy)
 new Vue({
   router,
   store,
+  i18n,
   render: h => h(App)
 }).$mount('#app')
--- client/vue.config.js
+++ client/vue.config.js
@@ -2,7 +2,17 @@ module.exports = {
   lintOnSave: false,
   filenameHashing: false,
   productionSourceMap: false,
+
   devServer: {
     proxy: 'http://localhost:5000'
+  },
+
+  pluginOptions: {
+    i18n: {
+      locale: 'en',
+      fallbackLocale: 'en',
+      localeDir: 'locales',
+      enableInSFC: true
+    }
   }
 }
