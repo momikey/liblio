c10b03badb360a232c898d6b8a8270249ed52b4e
Michael H. Potter <michael@potterpcs.net>
2019-09-03 19:20:06 -0400

    More installation stuff (BE)

--- server/setup.py
+++ server/setup.py
@@ -1,8 +1,10 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='liblio',
-    packages=['liblio'],
+    version='0.0.1',
+    author='Michael H. Potter',
+    packages=find_packages(),
     include_package_data=True,
     install_requires=[
         'flask'
