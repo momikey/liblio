28730fafb9d80bf50fa3facacc10c0499b7dbabd
Michael H. Potter <michael@potterpcs.net>
2019-09-16 02:22:31 -0400

    Following API endpoints (BE)

--- server/liblio/api/users.py
+++ server/liblio/api/users.py
@@ -4,10 +4,11 @@ from flask import Blueprint, current_app, jsonify, make_response
 from flask_jwt_extended import jwt_required, get_jwt_identity
 from webargs import fields, validate
 from webargs.flaskparser import parser
+from datetime import datetime
 
 from liblio import db, jwt
 from liblio.error import APIError
-from liblio.models import User
+from liblio.models import User, Login
 from . import API_PATH
 
 BLUEPRINT_PATH="{api}/user".format(api=API_PATH)
@@ -82,3 +83,76 @@ def get_posts_by_id(user_id):
         return jsonify([p.to_dict() for p in user.posts]), 200
     else:
         raise APIError(404, "User with ID {id} does not exist on this server".format(id=user_id))
+
+@blueprint.route('/followers/<int:user_id>', methods=('GET',))
+def get_user_followers(user_id):
+    """Get all of a user's followers."""
+    user = User.query.get(user_id)
+
+    if user is not None:
+        return jsonify([f.to_dict() for f in user.followers]), 200
+    else:
+        raise APIError(404, "User with ID {id} does not exist on this server".format(id=user_id))
+
+@blueprint.route('/following/<int:user_id>', methods=('GET',))
+def get_user_following(user_id):
+    """Get all users this user is following."""
+    user = User.query.get(user_id)
+
+    if user is not None:
+        return jsonify([f.to_dict() for f in user.following]), 200
+    else:
+        raise APIError(404, "User with ID {id} does not exist on this server".format(id=user_id))
+
+@blueprint.route('/follow/<int:user_id>', methods=('POST', 'PUT'))
+@jwt_required
+def follow_user(user_id):
+    """Follow the user with a given ID."""
+    username = get_jwt_identity()
+
+    login = Login.query.filter_by(username=username).first()
+
+    if login is None:
+        raise APIError(400, "User {username} does not exist on this server".format(username=username))
+
+    following_user = login.user
+
+    followed_user = User.query.get(user_id)
+    if followed_user is None:
+        raise APIError(404, "Attempting to follow a user who does not exist")
+
+    if user_id not in [u.id for u in following_user.following]:
+        following_user.following.append(followed_user)
+
+        login.last_action = datetime.now()
+        db.session.add(following_user, login)
+        db.session.commit()
+
+    return jsonify([u.to_dict() for u in following_user.following]), 201
+
+@blueprint.route('/follow/<int:user_id>', methods=('DELETE',))
+@jwt_required
+def unfollow_user(user_id):
+    """Stop following the user with a given ID."""
+    username = get_jwt_identity()
+
+    login = Login.query.filter_by(username=username).first()
+
+    if login is None:
+        raise APIError(400, "User {username} does not exist on this server".format(username=username))
+
+    following_user = login.user
+
+    followed_user = User.query.get(user_id)
+    if followed_user is None:
+        raise APIError(404, "Attempting to follow a user who does not exist")
+
+    follow_ids = [f.id for f in following_user.following]
+    if user_id in follow_ids:
+        del following_user.following[follow_ids.index(user_id)]
+
+        login.last_action = datetime.now()
+        db.session.add(following_user, login)
+        db.session.commit()
+
+    return jsonify([u.to_dict() for u in following_user.following]), 200
