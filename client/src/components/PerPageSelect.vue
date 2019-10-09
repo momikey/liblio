ef9bb69ea6b5e4db6d04948b8ed71fae989799e8
Michael H. Potter <michael@potterpcs.net>
2019-10-09 15:44:30 -0400

    Paginator and sketching out admin interface (FE)

---
+++ client/src/components/PerPageSelect.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="select">
+        <select name="per-page" :value="value" @input="$emit('input', $event.target.value)">
+            <option selected>10</option>
+            <option>25</option>
+            <option>100</option>
+        </select>
+
+        per page
+    </div>
+</template>
+
+<script>
+export default {
+    props: [
+        'value'
+    ]
+}
+</script>
--- client/src/views/admin-panel/Posts.vue
+++ client/src/views/admin-panel/Posts.vue
@@ -1,20 +1,120 @@
 <template>
     <section class="admin-main-panel">
-        <h1 v-if="mode == 'all'">All posts</h1>
-        <h1 v-else>Local posts</h1>
+        <div class="columns">
+            <div class="column">
+                <h1 class="is-title" v-if="mode == 'all'">{{ labels.allPosts }}</h1>
+                <h1 class="is-title" v-else>{{ labels.localOnly }}</h1>
+            </div>
+
+            <div class="column is-narrow">
+                <per-page-select v-model="perPage" />
+            </div>
+        </div>
+
+        <b-table
+            :data="data"
+            :columns="columns"
+            :default-sort="['id','asc']"
+            striped
+        >
+        </b-table>
     </section>
 </template>
 
 <script>
+import PerPageSelect from '@/components/PerPageSelect.vue';
+
 export default {
     data () {
         return {
+            perPage: 10,
 
+            // TODO: Change to Vuex/API call/whatever
+            data: [
+                {
+                    id: 42,
+                    user: "nobody",
+                    subject: "Filler",
+                    content: "Nothing to see here",
+                    flake: 1234567890,
+                    uri: "https://example.com/posts/abc123"
+                }
+            ],
+
+            labels: {
+                allPosts: "All posts",
+                localOnly: "Only local posts",
+                columns: {
+                    id: "ID",
+                    user: "User",
+                    subject: "Subject",
+                    content: "Content",
+                    flake: "Flake ID",
+                    uri: "URI",
+                }
+            },
+
+            columns: [],
         }
     },
 
     props: [
         'mode'
-    ]
+    ],
+
+    methods: {
+        labelFor (column) {
+            return this.labels.columns[column];
+        },
+
+        /*
+         * We can't access `this` in a data definition, which makes it
+         * much harder to define properly localized labels for columns.
+         * 
+         */
+        setupColumns () {
+            const columns = [
+                {
+                    label: this.labelFor('id'),
+                    field: "id",
+                    width: 40,
+                    numeric: true,
+                    centered: true,
+                    sortable: true
+                },
+                {
+                    label: this.labelFor('user'),
+                    field: "user",
+                    sortable: true
+                },
+                {
+                    label: this.labelFor('subject'),
+                    field: "subject"
+                },
+                {
+                    label: this.labelFor('content'),
+                    field: "content"
+                },
+                {
+                    label: this.labelFor('flake'),
+                    field: "flake"
+                },
+                {
+                    label: this.labelFor('uri'),
+                    field: "uri"
+                }
+            ];
+
+            this.columns = columns;
+        }
+    },
+
+    mounted () {
+        this.setupColumns();
+    },
+
+    components: {
+        PerPageSelect
+    }
 }
 </script>
