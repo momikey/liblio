c0e8c6317ee172848f8e83791f3ee681647c3e3e
Michael H. Potter <michael@potterpcs.net>
2019-10-14 19:27:34 -0400

    Admin tag display and more store methods (FE)

--- client/src/store/admin.js
+++ client/src/store/admin.js
@@ -9,13 +9,31 @@ export default module = {
     namespaced: true,
 
     state: {
+        /*
+         * Note that we store the count of all entities (user, post, tags),
+         * as a second data attribute, because we may only load a small portion
+         * of the entire set of these, but the front end needs to know the total
+         * count for pagination purposes.
+         */
         posts: [],
         postCount: 0,
+
+        tags: [],
+        tagCount: 0,
+
+        users: [],
+        userCount: 0,
     },
 
     getters: {
         posts: state => state.posts,
         postCount: state => state.postCount,
+
+        tags: state => state.tags,
+        tagCount: state => state.tagCount,
+
+        users: state => state.users,
+        userCount: state => state.userCount,
     },
 
     mutations: {
@@ -29,10 +47,35 @@ export default module = {
 
         setPostCount (state, count) {
             state.postCount = count;
-        }
+        },
+
+        setTags (state, newTags) {
+            state.tags = newTags;
+        },
+
+        clearTags (state) {
+            state.tags = [];
+        },
+
+        setTagCount (state, count) {
+            state.tagCount = count;
+        },
+
+        setUsers (state, newUsers) {
+            state.users = newUsers;
+        },
+
+        clearUsers (state) {
+            state.users = [];
+        },
+
+        setUserCount (state, count) {
+            state.userCount = count;
+        },
     },
 
     actions: {
+        // TODO: Fix up backend sorting for all of these
         getPosts ({ state, commit }, options) {
             const queryParams = {
                 max: options.max,
@@ -57,6 +100,36 @@ export default module = {
                 .catch(err => {
                     Snackbar.open({
                         message: `Unable to fetch posts: ${err.message}`,
+                        type: 'is-danger'
+                    });
+
+                    console.error(err);
+                })
+        },
+
+        getTags ({ state, commit }, options) {
+            const queryParams = {
+                max: options.max,
+                page: options.page,
+                sort: options.sort,
+                order: options.order
+            };
+
+            axios.get('/api/v1/admin/tags', {
+                params: queryParams,
+                headers: {
+                    'Authorization': `Bearer ${options.token}`
+                }
+            })
+                .then(r => {
+                    commit('setTags', r.data.tags);
+                    commit('setTagCount', r.data.total);
+
+                    return r;
+                })
+                .catch(err => {
+                    Snackbar.open({
+                        message: `Unable to fetch tags: ${err.message}`,
                         type: 'is-danger'
                     });
 
--- client/src/views/admin-panel/Tags.vue
+++ client/src/views/admin-panel/Tags.vue
@@ -1,12 +1,134 @@
 <template>
     <section class="admin-main-panel">
-        <h1>Tags</h1>
+        <div class="columns">
+            <div class="column">
+                <h1 class="is-title">{{ labels.title }}</h1>
+            </div>
+
+            <div class="column is-narrow">
+                <per-page-select v-model="perPage" />
+            </div>
+        </div>
+
+        <b-table
+            :data="data"
+            ref="table"
+            :default-sort="['id','asc']"
+            striped
+
+            detailed
+            :show-detail-icon="false"
+            detail-key="id"
+
+            paginated
+            backend-pagination
+            :total="totalTags"
+            :per-page="perPage"
+            @page-change="onPageChange"
+        >
+            <template v-slot="props">
+                <b-table-column field="id" :label="labels.columns.id" sortable
+                    width="40" numeric centered
+                >
+                    <span @click="toggleRow(props.row)">
+                        {{ props.row.id }}
+                    </span>
+                </b-table-column>
+
+                <b-table-column field="name" :label="labels.columns.name" sortable>
+                    <span @click="toggleRow(props.row)">
+                        {{ props.row.name }}
+                    </span>
+                </b-table-column>
+                
+                <b-table-column field="description" :label="labels.columns.description">
+                    <span>
+                        {{ props.row.description }}
+                    </span>
+                </b-table-column>
+            </template>
+
+            <template #detail="props">
+                <article class="columns">
+                    <div class="column is-three-quarters">
+                        <!-- TODO: Tag editor -->
+                    </div>
+                    <div class="column is-one-quarter">
+                        <!-- TODO: Add actions -->
+                    </div>
+                </article>
+            </template>
+        </b-table>
     </section>
 </template>
 
 <script>
-export default {
+import PerPageSelect from '@/components/PerPageSelect.vue';
+import { mapGetters } from 'vuex';
 
+export default {
+    data () {
+        return {
+            perPage: 10,
+            page: 1,
+
+            labels: {
+                title: "Tags",
+
+                columns: {
+                    id: "ID",
+                    name: "Name",
+                    description: "Description"
+                }
+            }
+        }
+    },
+
+    computed: {
+        ...mapGetters({
+            data: 'admin/tags',
+            totalTags: 'admin/tagCount',
+        }),
+    },
+
+    methods: {
+        fetchData () {
+            this.$store.dispatch('admin/getTags', {
+                max: +this.perPage,
+                page: this.page,
+                token: this.$store.getters.accessToken,
+            });
+        },
+
+        toggleRow (row) {
+            this.$refs.table.toggleDetails(row);
+        },
+
+        onPageChange (page) {
+            this.page = page;
+            this.fetchData();
+        },
+    },
+
+    watch: {
+        perPage () {
+            this.page = 1;
+            this.fetchData();
+        },
+
+        mode () {
+            this.fetchData();
+        }
+    },
+
+    mounted () {
+        this._mode = this.mode;
+        this.fetchData();
+    },
+
+    components: {
+        PerPageSelect
+    }
 }
 </script>
 
