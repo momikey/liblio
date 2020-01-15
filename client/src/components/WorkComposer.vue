8accf8c3c77128ded1b8286a72c6e5e43f90b790
Michael H. Potter <michael@potterpcs.net>
2020-01-15 13:31:58 -0500

    Started work composer and changed copyright year

---
+++ client/src/components/WorkComposer.vue
@@ -0,0 +1,73 @@
+<template>
+    <section class="composer work-composer card">
+        <div class="card-content">
+            <b-field horizontal :labels="labels.subject">
+                <b-input
+                    v-model="post.subject"
+                    :placeholder="labels.placeholder.subject"
+                />
+            </b-field>
+
+            <b-field horizontal :labels="labels.link">
+                <b-input
+                    v-model="post.link"
+                    :placeholder="labels.placeholder.link"
+                />
+            </b-field>
+
+            <!-- TODO: Optional post image -->
+
+            <b-field horizontal :labels="labels.body">
+                <b-input
+                    v-model="post.body"
+                    :placeholder="labels.placeholder.body"
+                    type="textarea"
+                />
+            </b-field>
+
+            <div class="composer-actions">
+                <!-- TODO: File uploads, tags, etc. -->
+            </div>
+        </div>
+    </section>
+</template>
+
+<script>
+export default {
+    data () {
+        return {
+            post: {
+                subject: '',
+                link: '',
+                body: ''
+            },
+
+            uploads: [],
+
+            labels: {
+                subject: "Subject",
+                link: "Link",
+                body: "Message body",
+
+                placeholders: {
+                    subject: "Here's what I'm working on",
+                    link: "https://example.com/my-greatest-work",
+                    body: "Tell the world about your creation."
+                },
+
+                errors: {
+
+                }
+            }
+        }
+    },
+
+    methods: {
+
+    }
+}
+</script>
+
+<style lang="scss">
+
+</style>
